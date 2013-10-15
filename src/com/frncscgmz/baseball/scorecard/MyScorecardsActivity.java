21525a9bc1aaa5c4e8cdbd639f53004f4aa5a065
Francisco Gomez <mail@frncscgmz.me>
2013-10-15 15:29:01 -0700

    Initial commit

---
+++ src/com/frncscgmz/baseball/scorecard/MyScorecardsActivity.java
@@ -0,0 +1,15 @@
+package com.frncscgmz.baseball.scorecard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyScorecardsActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
