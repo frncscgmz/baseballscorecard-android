ab1ec4eaaac752f9df452d4d7b2ac47649a74507
Francisco Gomez <mail@frncscgmz.me>
2013-10-24 00:01:58 -0700

    Menu items on my scorecards

--- src/com/frncscgmz/baseball/scorecard/MyScorecardsActivity.java
+++ src/com/frncscgmz/baseball/scorecard/MyScorecardsActivity.java
@@ -2,14 +2,21 @@ package com.frncscgmz.baseball.scorecard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MyScorecardsActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+   /** Called when the activity is first created. */
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
+   }
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu.myscorecards, menu);
+      return super.onCreateOptionsMenu(menu);
+   }
 }
